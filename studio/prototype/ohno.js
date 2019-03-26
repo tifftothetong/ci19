64f8c7f86b69e8b5a0469a92d0c75a3267761700
Tiffany Tong <tiffanytong@tiffanys-air.wireless.newschool.edu>
2019-03-26 12:17:14 -0400

    prototype

---
+++ studio/prototype/ohno.js
@@ -0,0 +1,29 @@
+function changeVideo() {
+
+  if (document.getElementByClassName("iframe").src == "https://www.youtube.com/embed/2xZp-GLMMJ0") 
+    {
+    document.getElementById("iframe").src = "https://www.youtube.com/watch?v=QvcYjSI_RyQ";
+    }
+  else 
+    {document.getElementById("iframe").src = "http://www.userinterfaceicons.com/80x80/minimize.png";
+      }
+    }
+
+ //
+
+// var newsrc = "mars.jpg";
+
+// function changeImage() {
+//   if ( newsrc == "mars.jpg" ) {
+//     document.images["pic"].src = "/images/program/js/forms/mars.jpg";
+//     document.images["pic"].alt = "Mars";
+//     newsrc  = "earth.jpg";
+//   }
+//   else {
+//     document.images["pic"].src = "/images/program/js/forms/earth.jpg";
+//     document.images["pic"].alt = "Earth";
+//     newsrc  = "mars.jpg";
+//   }
+
+//   //
+
